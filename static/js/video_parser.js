ff1e2f25f561e416c1c659e7154952c2dfebbf23
Davide <davide@MacBook-Pro-di-Davide-2.local>
2026-05-27 22:00:48 +0200

    fix h264 winwdows

--- static/js/video_parser.js
+++ static/js/video_parser.js
@@ -3,6 +3,7 @@ class VideoParser {
         this.debug = debug
         this.buffer = new Uint8Array(0);
         this.name = null;
+        this.codecId = null;
         this.width = null;
         this.height = null;
         this.hasKeyFrame = null;
@@ -11,6 +12,39 @@ class VideoParser {
         this.mimeCodec = null;
         this.onNaluCallback = onNaluCallback;
         this.hasSentSpsPps = false;
+    }
+
+    static get CODEC_IDS() {
+        return new Map([
+            [0x68323634, 'h264'],
+            [0x68323635, 'h265'],
+            [0x00617631, 'av1']
+        ]);
+    }
+
+    isValidSize(width, height) {
+        return Number.isInteger(width) &&
+            Number.isInteger(height) &&
+            width > 0 &&
+            height > 0 &&
+            width <= 10000 &&
+            height <= 10000;
+    }
+
+    readUint32(offset) {
+        return new DataView(this.buffer.buffer, this.buffer.byteOffset, this.buffer.byteLength).getUint32(offset, false);
+    }
+
+    sendSize(width, height) {
+        this.width = width;
+        this.height = height;
+        console.log("width:" + this.width + " height:" + this.height);
+        if (this.onNaluCallback) {
+            this.onNaluCallback({
+                type: 'screen_size',
+                data: { "width": this.width, "height": this.height }
+            });
+        }
     }
 
     appendData(data) {
@@ -23,10 +57,24 @@ class VideoParser {
 
     scrcpyProcessBuffer() {
         let startIndex = 0;
-        if (this.name == null) {
-            if (this.buffer.length >= 64) {
+        while (true) {
+            if (this.name == null) {
+                if (this.buffer.length < 4) {
+                    break;
+                }
+
+                const maybeCodecId = this.readUint32(0);
+                if (VideoParser.CODEC_IDS.has(maybeCodecId)) {
+                    this.name = '';
+                    continue;
+                }
+
+                if (this.buffer.length < 64) {
+                    break;
+                }
+
                 const name = this.buffer.slice(0, 64);
-                this.name = new TextDecoder().decode(name);
+                this.name = new TextDecoder().decode(name).replace(/\0+$/g, '');
                 console.log("Device name:" + this.name);
                 if (this.onNaluCallback) {
                     this.onNaluCallback({
@@ -35,26 +83,86 @@ class VideoParser {
                     });
                 }
                 startIndex = 64;
-            }
-        } else if (this.width == null) {
-            if (this.buffer.length >= 12) {
-                const id = new DataView(this.buffer.buffer).getInt32(0, false);
-                this.width = new DataView(this.buffer.buffer).getInt32(4, false);
-                this.height = new DataView(this.buffer.buffer).getInt32(8, false);
-                console.log("width:" + this.width + " height:" + this.height);
-                if (this.onNaluCallback) {
-                    this.onNaluCallback({
-                        type: 'screen_size',
-                        data: { "width": this.width, "height": this.height }
-                    });
+                break;
+            } else if (this.codecId == null) {
+                if (this.buffer.length < 4) {
+                    break;
                 }
-                startIndex += 12;
-            }
-        } else while (this.buffer.length - startIndex > 12) {
-            // const flag = new DataView(this.buffer.buffer).getInt64(0, false);
 
-            const size = new DataView(this.buffer.buffer).getInt32(startIndex + 8, false);
-            if (this.buffer.length - startIndex >= 12 + size) {
+                const codecId = this.readUint32(0);
+                if (!VideoParser.CODEC_IDS.has(codecId)) {
+                    console.warn("Unknown video codec id", codecId.toString(16));
+                    break;
+                }
+
+                this.codecId = VideoParser.CODEC_IDS.get(codecId);
+                console.log("Video codec:" + this.codecId);
+                startIndex = 4;
+                break;
+            } else if (this.width == null) {
+                if (this.buffer.length < 8) {
+                    break;
+                }
+
+                const firstByte = this.buffer[0];
+                if ((firstByte & 0x80) !== 0) {
+                    if (this.buffer.length < 12) {
+                        break;
+                    }
+                    const width = this.readUint32(4);
+                    const height = this.readUint32(8);
+                    if (!this.isValidSize(width, height)) {
+                        console.warn("Ignoring invalid scrcpy session size", width, height);
+                        break;
+                    }
+                    this.sendSize(width, height);
+                    startIndex = 12;
+                } else if (VideoParser.CODEC_IDS.has(this.readUint32(0))) {
+                    const width = this.readUint32(4);
+                    const height = this.readUint32(8);
+                    if (!this.isValidSize(width, height)) {
+                        console.warn("Ignoring invalid scrcpy stream size", width, height);
+                        break;
+                    }
+                    this.codecId = VideoParser.CODEC_IDS.get(this.readUint32(0));
+                    this.sendSize(width, height);
+                    startIndex = 12;
+                } else {
+                    const width = this.readUint32(0);
+                    const height = this.readUint32(4);
+                    if (!this.isValidSize(width, height)) {
+                        break;
+                    }
+                    this.sendSize(width, height);
+                    startIndex = 8;
+                }
+                break;
+            } else {
+                break;
+            }
+        }
+
+        if (startIndex > 0 && this.width == null) {
+            this.buffer = this.buffer.slice(startIndex);
+            this.scrcpyProcessBuffer();
+            return;
+        }
+
+        while (this.width != null && this.buffer.length - startIndex > 12) {
+            const firstByte = this.buffer[startIndex];
+
+            if ((firstByte & 0x80) !== 0 && (firstByte & 0x40) === 0) {
+                const width = new DataView(this.buffer.buffer, this.buffer.byteOffset, this.buffer.byteLength).getUint32(startIndex + 4, false);
+                const height = new DataView(this.buffer.buffer, this.buffer.byteOffset, this.buffer.byteLength).getUint32(startIndex + 8, false);
+                if (this.isValidSize(width, height)) {
+                    this.sendSize(width, height);
+                    startIndex += 12;
+                    continue;
+                }
+            }
+
+            const size = new DataView(this.buffer.buffer, this.buffer.byteOffset, this.buffer.byteLength).getUint32(startIndex + 8, false);
+            if (size > 0 && this.buffer.length - startIndex >= 12 + size) {
                 const nalu = this.buffer.slice(startIndex + 12, startIndex + 12 + size);
                 this.processBuffer(nalu)
                 startIndex = startIndex + 12 + size;
